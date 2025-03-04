de1f8975f9d3f83cf938ac6d54ae2b78f512e65c
Vebjørn Gilberg <vebgil@netcompany.com>
2025-03-04 12:42:08 +0100

    add tfvars

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,13 @@
 module "networking" {
   source         = "../../modules/networking"  # Path to networking module
-  project_id     = var.project_id
+  project_id     = var.network_project_id
   network_name   = var.network_name
   #subnet_names   = var.subnet_names  # List of subnet names
 }
 
 resource "google_compute_instance" "test_vm" {
     name         = "test-vm"
-    machine_type = "e2-micro"
+    machine_type = var.vm_instance_type # e2-micro
     zone         = "europe-west1-b"
     boot_disk {
         initialize_params {
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,11 +1,29 @@
 variable "network_project_id" {
   description = "GCP project ID for networking resources"
   type        = string
-  default     = "shared-vpc-network"
+  default     = "shared-vpc"
+}
+
+variable "network_name" {
+  description = "The name of the VPC network being created"
+  type        = string
+  default     = "shared-vpc"
 }
 
 variable "app_project_id" {
   description = "GCP project ID for application resources"
   type        = string
   default     = "app-project"
+}
+
+variable "region" {
+  description = "The region to create the VPC network in"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "vm_instance_type" {
+  description = "The type of VM instance to create"
+  type        = string
+  default     = "e2-micro"
 }
